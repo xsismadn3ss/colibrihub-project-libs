c8cd1efecbff1ffc0032304b403612d76dfbfe42
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-09 12:28:10 -0600

    📝 Agregar documentación para  los  guards

--- projects/shared-components/src/lib/users/session/is-logged-in-guard.ts
+++ projects/shared-components/src/lib/users/session/is-logged-in-guard.ts
@@ -3,7 +3,11 @@ import {inject} from '@angular/core';
 import {ValidationService} from 'colibrihub-shared-services';
 import {catchError, map, of} from 'rxjs';
 
-
+/**
+ * Guard de tipo match para determinar si el usuario ha iniciado sesión
+ * @param route
+ * @param segments
+ */
 export const isLoggedInGuard: CanMatchFn = (route, segments) => {
   const validation = inject(ValidationService);
 
--- projects/shared-components/src/lib/users/session/is-logged-out-guard.ts
+++ projects/shared-components/src/lib/users/session/is-logged-out-guard.ts
@@ -3,6 +3,11 @@ import {inject} from '@angular/core';
 import {ValidationService} from 'colibrihub-shared-services';
 import {catchError, map, of} from 'rxjs';
 
+/**
+ * Guard de tipo match para determinar si el usuario **no** ha iniciado sesión
+ * @param route
+ * @param segments
+ */
 export const isLoggedOutGuard: CanMatchFn = (route, segments) => {
   const validation = inject(ValidationService)
 

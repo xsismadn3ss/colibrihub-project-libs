eda021cc5575a2f145603fe187d237bf84e4662c
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-10 23:17:19 -0600

    eliminar token de inyección no utilizado

--- projects/shared-services/src/lib/config/config.ts
+++ projects/shared-services/src/lib/config/config.ts
@@ -1,13 +1,6 @@
 import {InjectionToken} from '@angular/core';
 
 /**
- * URL para inicio de sesión, es necesaria para indicar
- * a que URL se hará la redirección para iniciar sesión
- * en un módulo externo
- */
-export const AUTH_LOGIN_URL = new InjectionToken<string>('AUTH_LOGIN_URL');
-
-/**
  * URL del microservicio de autenticación
  * <br>
  * Es utilizada por ``AuthService`` y ``ValidationService``

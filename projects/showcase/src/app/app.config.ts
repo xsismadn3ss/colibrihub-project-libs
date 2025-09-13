b0157eb05a2670834c523e94819cede1d31e25e3
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-09-13 12:15:08 -0600

    crear proyecto para probar componentes

---
+++ projects/showcase/src/app/app.config.ts
@@ -0,0 +1,7 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+  ]
+};
---
+++ projects/showcase/src/app/app.ts
@@ -0,0 +1,11 @@
+import {Component, inject, OnInit, signal} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+
+}
